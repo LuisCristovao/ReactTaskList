2008e6089386e950667780f39e841e70078891d9
Luis C <luis.cristovao92@gmail.com>
2025-03-07 17:17:33 +0000

    update

--- taskList/src/App.tsx
+++ taskList/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const list_class = "cool-list";
+  const tasks = ["1", "2", "333"];
+  // State to track which list item is hovered
+  const [listIndex, setlistIndex] = useState<number | null>(null);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Tasks</h1>
+      <ul className={list_class}>
+        {tasks.map((task, index) => (
+          <li
+            key={index}
+            className={list_class}
+            onMouseOver={() => {
+              setlistIndex(index);
+              console.log(listIndex);
+            }} // Set hover index
+          >
+            {task}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
