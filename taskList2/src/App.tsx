dd165f2c9470dc9da7d6128b2f97975a2ac2de44
Luis C <luis.cristovao92@gmail.com>
2025-03-10 15:51:40 +0000

    u

--- taskList2/src/App.tsx
+++ taskList2/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let tasks = ["sssss", "aaaa", "oooooo"];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Tasks</h1>
+      <ul className="cool-list">
+        {tasks.map((task, index) => {
+          return (
+            <li
+              key={index}
+              className="cool-list"
+              onMouseOver={() => {
+                return console.log(task, index);
+              }}
+            >
+              {task}
+            </li>
+          );
+        })}
+        <li 
+        key="new-task" 
+        className="cool-list"
+        onClick={(e)=>{
+          
+          e.target.outerHTML=`<input type="text" autoFocus onBlur="(this)=>{tasks.append(this.value)}" \>`
+          // console.log(e)
+        }}
+        >+</li>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
